710fc1ce7355f2294cc45842b6648f9802a845fa
kintautas <kintautas@gmail.com>
2017-07-26 17:33:33 +0300

    Added password hasing and database

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const express = require('express')
 const app = express()
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/jobs');
+var password = require('password-hash-and-salt');
+
 
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({
@@ -13,10 +15,33 @@ app.use(bodyParser.urlencoded({
  */
 app.use(bodyParser.json());
 
+var User = mongoose.model('User', { email: String, password: String });
+
+
+
 
 app.post('/register', function (req, res) {
+
 	console.log('Registering new user');
 	console.log(req.body.email)
+	console.log(req.body.password)
+	var email = req.body.email;
+	var inputPassword = req.body.password;
+	var hashed;
+	password(inputPassword).hash(function(error, hash) {
+    if(error)
+        throw new Error('Something went wrong!');
+    	hashed = hash;
+    	var user = new User({ email:email, password:hashed });
+		user.save(function (err) {
+		  if (err) {
+		    console.log(err);
+		  } else {
+		    console.log('user saved');
+		  }
+		});
+
+    })
   res.send('Hello World!')
 })
 
